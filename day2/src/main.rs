9558a1f42b8c23929d7c301261ab5fb81632ca2e
jesse-rb <jesse.reynekebarnard@gmail.com>
2024-12-03 23:30:18 +1300

    fix: day2pt1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,8 +5,7 @@ fn main() {
 
     let part1 = part1(&lines);
 
-    println!("part1: {}", part1)
-
+    println!("part1: NUM OF SAFE REPORTS {}", part1);
 }
 
 fn get_input() -> Vec<String> {
@@ -19,42 +18,55 @@ fn part1(lines: &Vec<String>) -> i32 {
     let mut num_unsafe = 0;
     let mut total = 0;
 
-    for line in lines {
+    for report in lines {
         let mut prev: Option<i32> = None;
         let mut decreasing: Option<bool> = None;
 
-        for part in line.split_whitespace() {
+        // Traverse over line
+        println!("---STARTING---");
+        for part in report.split_whitespace() {
+            // must be i32
             let curr = part.parse::<i32>().expect("input broken");
-    
-            let mut safe = true;
             
+            // Assume SAFE to start with
+            let mut safe = true;
+
             if prev.is_some() {
-                if prev == Some(curr) {
-                    safe = false;
+                let diff = prev.unwrap() - curr;
+                let diff_abs = diff.abs();
+
+
+                safe = safe && diff_abs > 0;
+                safe = safe && diff_abs <= 3;
+
+                if decreasing.is_none() {
+                    decreasing = Some(diff > 0);
                 }
-                else if decreasing.is_some() {
-                    if decreasing == Some(true) {
-                        safe = (prev < Some(curr)) && (prev >= Some(curr - 3));
+
+                if decreasing.is_some() {
+                    if decreasing.unwrap() {
+                        safe = safe && diff > 0;
                     }
                     else {
-                        safe = (prev > Some(curr)) && (prev <= Some(curr + 3));
+                        safe = safe && diff < 0;
                     }
                 }
-            }
 
-            if decreasing.is_none() {
-                decreasing = Some(prev > Some(curr));
+                println!("prev: {}, curr: {}, diff: {}, diff_abs: {}, decreasing: {}", prev.unwrap(), curr, diff, diff_abs, decreasing.unwrap());
             }
 
             prev = Some(curr);
 
-            total += 1;
             if !safe {
+                println!("---FOUND UNSAFE---");
                 num_unsafe += 1;
                 break;
             }
         }
+        total += 1;
     }
 
+    println!("TOTAL REPORTS: {}", total);
+    println!("TOTAL UNSAFE REPORTS: {}", num_unsafe);
     total - num_unsafe
 }
