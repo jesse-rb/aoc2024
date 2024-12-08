6501d8119e7078f95a608e0daf6e31a74f4a4ee6
jesse-rb <jesse.reynekebarnard@gmail.com>
2024-12-08 13:32:16 +1300

    Day 3 (Part 2)

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -6,10 +6,10 @@ fn main() {
     let input = get_input();
 
     let part1 = part1(&input);
-    //let part2 = part2(&lines);
+    let part2 = part2(&input);
 
     println!("part1: {}", part1);
-    //println!("part2: {}", part2);
+    println!("part2: {}", part2);
 }
 
 fn get_input() -> String {
@@ -34,6 +34,51 @@ fn part1(input: &str) -> i32 {
 
     for m in matches {
         let (a, b) = m.unwrap();
+
+        //println!("tuple: ({}, {})", a, b);
+
+        total += a * b;
+    }
+
+    total
+}
+
+fn part2(input: &str) -> i32 {
+    let mut total: i32 = 0;
+
+    let pattern: &str = r"(?x) # Verbose mode for comments and whitespace (readability)
+        (?P<do>do\(\))
+        |(?P<dont>don't\(\))
+        |(?P<mul>mul\((\d+),(\d+)\))
+    ";
+    let re = Regex::new(pattern).expect("Invalid regex");
+    let matches = re.captures_iter(input);
+
+    let mut is_do: bool = true;
+    let mut to_sum:Vec<(i32, i32)> = Vec::new();
+
+    for m in matches {
+        if m.name("do").is_some() {
+            println!("found do");
+            is_do = true;
+        }
+        else if m.name("dont").is_some() {
+            println!("found don't");
+            is_do = false;
+        }
+        else if m.name("mul").is_some() && is_do {
+            println!("found mul");
+            let a:Option<i32> = m.get(4).and_then(|r| r.as_str().parse::<i32>().ok());
+            let b:Option<i32> = m.get(5).and_then(|r| r.as_str().parse::<i32>().ok());
+
+            if let (Some(some_a), Some(some_b)) = (a, b) {
+                to_sum.push((some_a, some_b));
+            }
+        }
+    }
+
+    for m in to_sum {
+        let (a, b) = m;
 
         println!("tuple: ({}, {})", a, b);
 
