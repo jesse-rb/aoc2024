b31d63b941fd6f3e4979e152796d50d01962f287
jesse-rb <jesse.reynekebarnard@gmail.com>
2024-12-25 12:43:27 +1300

    Add launch.json and small cargo toml adjustments to enabled step debugging

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -3,8 +3,6 @@ use std::io::{self, Read};
 use regex::Regex;
 
 fn main() {
-    println!("Hello day 5!");
-
     let (rules, updates) = get_input();
 
     let part1 = part1(&rules, &updates);
@@ -20,19 +18,13 @@ fn get_input() -> (String, Vec<Vec<String>>) {
 
     let mut parts = input.split("\n\n");
 
-    let rules = parts
-        .next()
-        .unwrap_or("").to_string();
+    let rules = parts.next().unwrap_or("").to_string();
 
     let updates = parts
         .next()
         .unwrap_or("")
         .split("\n")
-        .map(
-            |s| s.split(",")
-                .map(|d| d.to_string())
-                .collect()
-        )
+        .map(|s| s.split(",").map(|d| d.to_string()).collect())
         .collect();
 
     (rules, updates)
@@ -53,22 +45,25 @@ fn part1(rules: &str, updates: &[Vec<String>]) -> i32 {
             println!("--\ncurrent: {}", page);
             if processed_pages.is_empty() {
                 // Special case: safe by default if first page in the update sequence
-            }
-            else {
+            } else {
                 let pattern_before: &str = &format!(r"([0-9]+)\|{}", page);
                 let pattern_after: &str = &format!(r"{}\|([0-9]+)", page);
                 let re_before = Regex::new(pattern_before).expect("Invalid pattern");
                 let re_after = Regex::new(pattern_after).expect("Invalid pattern");
 
-                let matched_must_befores = re_before.captures_iter(rules).filter_map(|m| m.get(1).map(|m| m.as_str()));
-                let matched_must_afters = re_after.captures_iter(rules).filter_map(|m| m.get(1).map(|m| m.as_str()));
+                let matched_must_befores = re_before
+                    .captures_iter(rules)
+                    .filter_map(|m| m.get(1).map(|m| m.as_str()));
+                let matched_must_afters = re_after
+                    .captures_iter(rules)
+                    .filter_map(|m| m.get(1).map(|m| m.as_str()));
 
                 let mut one_before_rule_is_safe = false;
 
                 for some_must_before in matched_must_befores {
                     println!("must be before: {:?}", some_must_before);
                     println!("processed: {:?}", processed_pages);
-                    
+
                     if processed_pages.contains(&some_must_before.to_string()) {
                         one_before_rule_is_safe = true;
                         break; // Break out early
@@ -95,17 +90,21 @@ fn part1(rules: &str, updates: &[Vec<String>]) -> i32 {
             processed_pages.push(page.to_string());
         }
 
-
         println!("update: {:?}\nsafe: {}", update, safe);
 
         if safe {
-            let middle_index = processed_pages.len()/2;
+            let middle_index = processed_pages.len() / 2;
             let middle = processed_pages[middle_index].parse::<i32>().unwrap_or(0);
 
             total += middle;
         }
     }
-    
+
+    total
+}
+
+fn part2() -> i32 {
+    let mut total: i32 = 0;
 
     total
 }
